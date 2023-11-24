37fefcbab3a5ad1b15d40da9bf24504bb4768b98
valleoict <145077524+valleoict@users.noreply.github.com>
2023-11-24 01:41:32 -0800

    subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,6 @@
 # Create a public subnet
 resource "aws_subnet" "public" {
-  count                   = 4
+  count                   = 2
   vpc_id                  = aws_vpc.custom.id
   cidr_block              = var.public_subnet_cidr[count.index]
   map_public_ip_on_launch = true
