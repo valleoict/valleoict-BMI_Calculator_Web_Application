3c6c993ad37fe8a5f74faef41ed0ba70d1079528
konanhal2002 <konanhal2002@yahoo.co.uk>
2023-11-25 20:38:17 +0000

    created an routeassociation.tf file

---
+++ routeassociation.tf
@@ -0,0 +1,5 @@
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.route.id
+}
