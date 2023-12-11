30b6f37f331a6155ed42f5ce9e095e8c1467e8f7
konanhal2002 <144851679+konanhal2002@users.noreply.github.com>
2023-12-11 22:42:15 +0000

    Update autoscaling_group.tf

--- autoscaling_group.tf
+++ autoscaling_group.tf
@@ -4,7 +4,7 @@ resource "aws_autoscaling_group" "autoscaling" {
   min_size                  = 1
   health_check_type         = "ELB"
   health_check_grace_period = 300
-  launch_configuration      = aws_launch_configuration.launch_config.id
+  launch_configuration      = aws_launch_configuration.launch_config_01.id
   vpc_zone_identifier       = aws_subnet.public[*].id
 
   target_group_arns = [aws_lb_target_group.alb-target-group.arn]
