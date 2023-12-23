91363d190c860007091f382c1d85ea034d04ff4b
Dorcas Agbakwuru <144885958+DorcasChidinma@users.noreply.github.com>
2023-12-23 23:29:03 +0100

    Update ec2_security_group.tf

--- ec2_security_group.tf
+++ ec2_security_group.tf
@@ -8,7 +8,21 @@ resource "aws_security_group" "instance_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.sg.id]
+  }
+
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.sg.id]
+  }
+
+   ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    security_groups = [aws_security_group.sg.id]
   }
 
   egress {
